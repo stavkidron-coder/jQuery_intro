6ba99f715f999445fccbfbb07a93440bbc2cf677
Stav Kidron <kidronstav@gmail.com>
2020-10-01 14:28:26 -0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+console.log('Hello World');
+
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('hello from jQ');
+}
+
+function hello(){
+    console.log('helloooooo');
+}
+
+function goodbye(){
+    console.log('goodbyeeee');
+}
+
+hello();
+goodbye();

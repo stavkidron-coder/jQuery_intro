e0807b232304d00c143fa86d929d294d4eb89217
Stav Kidron <kidronstav@gmail.com>
2020-10-02 11:02:05 -0500

    added pinkColor and changeColor btn

--- scripts.js
+++ scripts.js
@@ -2,12 +2,38 @@ console.log('Hello World');
 
 $(document).ready(onReady);
 
+let favFoods = ["Pizza", "Sushi", "Burritos", "Tacos"];
+
 function onReady(){
     console.log('hello from jQ');
     //event listener
-    $('#clickMe').on('click', buttonClicked)
+    $('#clickMe').on('click', buttonClicked);
+
+    //descendant selector
+    $('#favoriteFoods').on('click', '.deleteBtn', deleteFunc);
+
+    $('#favoriteFoods').on('click', '.changeColor', changeColor);
+}
+
+function deleteFunc(){
+    console.log('Delete');
+    $(this).parent().remove();
+}
+
+function changeColor(){
+    console.log('Color Changed');
+    $(this).parent().addClass('pinkColor');
 }
 
 function buttonClicked(){
-    console.log('I\'m Clicked');  
+    console.log('I\'m Clicked');
+    let ulFoods = $("#favoriteFoods");
+    for (let i = 0; i < favFoods.length; i++) {
+        ulFoods.append(`
+            <li> One of my favorite foods is ${favFoods[i]} 
+                <button class="deleteBtn">Delete</button>
+                <button class="changeColor">Change Color</button>
+            </li>
+        `);   
+    } 
 }

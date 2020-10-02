36dfb55b660f1b448890f0599a96a629d40051bf
Stav Kidron <kidronstav@gmail.com>
2020-10-02 13:40:16 -0500

    added counter

--- scripts.js
+++ scripts.js
@@ -3,16 +3,21 @@ console.log('Hello World');
 $(document).ready(onReady);
 
 let favFoods = ["Pizza", "Sushi", "Burritos", "Tacos"];
+let employees = [];
+let count =0;
 
 function onReady(){
     console.log('hello from jQ');
     //event listener
     $('#clickMe').on('click', buttonClicked);
+    $('#submitForm').on('click', submitForm);
+    $('#counter').on('click', countUp);
+    $('#count').text(count);
 
     //descendant selector
     $('#favoriteFoods').on('click', '.deleteBtn', deleteFunc);
-
     $('#favoriteFoods').on('click', '.changeColor', changeColor);
+    
 }
 
 function deleteFunc(){
@@ -21,19 +26,51 @@ function deleteFunc(){
 }
 
 function changeColor(){
-    console.log('Color Changed');
-    $(this).parent().addClass('pinkColor');
+    $(this).parent().toggleClass('pinkColor');
+}
+
+function countUp(){
+    console.log('count up');
+    count++;
+    $('#count').text(count);
+}
+
+function submitForm(){
+    // grab input values
+    //getter .val()
+    //setter .val('something');
+    // .text() is another getter/setter
+    
+    let name = $('#name').val(); //creates a name variable that takes the value of name input
+    console.log('Name:', name); //logs the inputed name
+    
+    let title = $('#title').val(); //creates a name variable that takes the value of name input
+    console.log('Title:', title); //logs the inputed title
+    //employees.push(title); // pushes name input into emplyees array
+
+    let employeeObj = {
+        name: name,
+        title: title
+    };
+
+    employees.push(employeeObj);
+
+    $('#title').val(''); // resets value as an empty string
+    $('#name').val(''); // resets value as an empty string
 }
 
 function buttonClicked(){
     console.log('I\'m Clicked');
     let ulFoods = $("#favoriteFoods");
+    //clear out ul to append a fresh list
+    ulFoods.empty();
+
     for (let i = 0; i < favFoods.length; i++) {
         ulFoods.append(`
             <li> One of my favorite foods is ${favFoods[i]} 
                 <button class="deleteBtn">Delete</button>
                 <button class="changeColor">Change Color</button>
             </li>
-        `);   
+        `);
     } 
 }
